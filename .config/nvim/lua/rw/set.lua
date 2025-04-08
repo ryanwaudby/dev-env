c88b2f82ff9d7755e352026a63976bd37af6eed8
ryanwaudby <ryan.waudby@bbc.co.uk>
2025-04-08 11:47:55 +0100

    Add diganotic for nvim

--- .config/nvim/lua/rw/set.lua
+++ .config/nvim/lua/rw/set.lua
@@ -38,3 +38,6 @@ for type, icon in pairs({
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
+
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostics' })
+
